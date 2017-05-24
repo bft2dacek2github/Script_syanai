13822bb220e057868d24a31b2ee57cb9dd42f456
Dacek <d-miura+github@bfts.co.jp>
2017-05-24 23:21:58 +0900

    新規作成

---
+++ work001.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+dim INPT
+
+echo "文字列を入力してください："
+read INPT
+
+expr "$INPT" + 1 >/dev/null 2>&1
+if [ $? -lt 2 ]
+then
+    echo "入力された文字列は数値です。"
+else
+    echo "入力された文字列は数値ではありません。"
+fi
+
